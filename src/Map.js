abe8a99e94ad452b53c97cfa6d26a9f37b7d9ce3
Anders Mogensen <am@mapspeople.com>
2021-03-05 10:05:31 +0100

    Conform to new interface for map component.

--- src/Map.js
+++ src/Map.js
@@ -17,12 +17,12 @@ function Map() {
   const mapElementReference = useRef(null); // We need a reference to the mi-map in order to add event listeners.
 
   useEffect(() => { // The function passed to useEffect will run after the render is committed to the screen.
-      mapElementReference.current.addEventListener('mapsIndoorsReady', () => {
-        mapElementReference.current.panTo({lat: 38.8979504120527, lng: -77.03665279788513});
-      });
+    mapElementReference.current.addEventListener('mapsIndoorsReady', () => {
+      mapElementReference.current.getMapInstance().then(mapInstance => mapInstance.setCenter({ lat: 38.8974905, lng: -77.0362723 }));
 
-      mapElementReference.current.addEventListener('locationClicked', location => {
-        setLocationAndPage(location.detail);
+      mapElementReference.current.getMapsIndoorsInstance().then(mapsIndoorsInstance => {
+        mapsIndoorsInstance.addListener('click', location => setLocationAndPage(location));
+      });
     });
   });
 
@@ -41,7 +41,7 @@ function Map() {
   }
 
   return <div className="map">
-    <mi-map ref={mapElementReference} zoom="18" position-control="TOP_CENTER" mi-api-key="demo" gm-api-key="AIzaSyBNhmxW2OntKAVs7hjxmAjFscioPcfWZSc" floor-selector="RIGHT_CENTER"></mi-map>
+    <mi-map-googlemaps ref={mapElementReference} zoom="18" my-position-control-position="TOP_CENTER" mi-api-key="demo" gm-api-key="AIzaSyBNhmxW2OntKAVs7hjxmAjFscioPcfWZSc" floor-selector-control-position="RIGHT_CENTER"></mi-map-googlemaps>
     {page !== pages.NONE &&
       <mi-card>
         <button className="card-close" onClick={() => close()}>&mdash;</button>
